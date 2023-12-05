efefdbced3ce01ff234eab9b2c7809722ca908d4
entropy <immansounthon@gmail.com>
2023-12-05 10:08:02 +0000

    Initial commit
    solution to day 1 part 1

---
+++ day 1/scripts/part-1.py
@@ -0,0 +1,12 @@
+import re;
+
+puzzleInput = open("advent-of-code/day 1/puzzle-input.md", "r");
+
+def solution():
+    n = ("".join(re.findall("[0-9]+", puzzleInput.readline())));
+    return(int(n[0] + n[-1]));
+
+n = 0;
+for i in range(0, 1000):
+    n += solution();
+    print(n);
