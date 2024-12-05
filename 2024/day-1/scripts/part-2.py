223ecdfa7ae4856435a933c114ba8d0d6b037177
immy soun <immansounthon@gmail.com>
2024-12-05 09:46:13 +0000

    day 1 complete

--- 2024/day-1/scripts/part-2.py
+++ 2024/day-1/scripts/part-2.py
@@ -8,15 +8,12 @@ for i in range(1000):
     list1.append(int(line[0]));
     list2.append(int(line[1]));
 
-print(list1);
-print(list2);
-
 list1.sort();
 list2.sort();
 
 total = 0;
 for i in range(len(list1)):
-    diff = abs(list2[i] - list1[i]);
+    diff = list1[i]*list2.count(list1[i]);
     total += diff;
 
 print(total);
